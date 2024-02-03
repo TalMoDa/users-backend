5d35236d46426b67c8bea9061b4ad5393e64eaea
Roie <roihemo32321@gmail.com>
2024-02-03 19:39:29 +0200

    Pushed the last commit to git

---
+++ users-backend/CrudAPI/Controllers/UserController.cs
@@ -0,0 +1,74 @@
+﻿using CrudAPI.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CrudAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly DataContext _context;
+        public UserController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<List<User>>> GetUserById(int id)
+        {
+            return Ok(await _context.Users.FindAsync(id));
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<User>>> GetUsers()
+        {
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<List<User>>> CreateUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpPut]
+        public async Task<ActionResult<List<User>>> UpdateUser(User user)
+        {
+            var dbUser = await _context.Users.FindAsync(user.Id);
+            if (dbUser == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            dbUser.Name = user.Name;
+            dbUser.Phone = user.Phone;
+            dbUser.Email = user.Email;
+
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<List<User>>> DeleteUser(int id)
+        {
+            var dbUser = await _context.Users.FindAsync(id);
+            if (dbUser == null)
+            {
+                return BadRequest("User not found");
+            }
+
+            _context.Users.Remove(dbUser);
+            await _context.SaveChangesAsync();
+
+            return Ok(await _context.Users.ToListAsync());
+
+        }
+    }
+}
---
+++ users-backend/CrudAPI/Program.cs
@@ -0,0 +1,42 @@
+using CrudAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Created the connection between EF to the sql database.
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+// Accepting and giving permissions to our  client to the server.
+builder.Services.AddCors(options => options.AddPolicy(name: "UserOrigins", policy =>
+{
+    policy.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader();
+}));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+// Here we are just using the cors we addded to our application.
+app.UseCors("UserOrigins");
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
